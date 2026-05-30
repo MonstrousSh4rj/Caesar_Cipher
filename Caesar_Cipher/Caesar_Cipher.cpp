e9c77802c9db00fb8923aeea8fbb64f9ddfd071c
MonstrousSh4rj <sebastain6789@gmail.com>
2026-05-30 04:12:48 -0700

    Added Encryption

--- Caesar_Cipher/Caesar_Cipher.cpp
+++ Caesar_Cipher/Caesar_Cipher.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
-#include<string>
+#include<cstring>
 using namespace std;
-void user_input(string &text, int &shift);
+void user_input(char text[], int& shift);
+void encryption(char text[], int shift);
 int main()
 {
     int choice;
@@ -23,15 +24,32 @@ int main()
         }
     } 
     while (choice < 1 || choice > 4);
-	string text;
+	char text[100];
 	int shift;
 	user_input(text, shift);
+    encryption(text, shift);
 }
-void user_input(string &text, int &shift)
+void user_input(char text[], int& shift)
 {
     cin.ignore();
-	cout << "Please Enter your text: ";
-	getline(cin, text);
-	cout << "Please Enter number to shift: ";
-	cin >> shift;
+    cout << "Please Enter your text: ";
+    cin.getline(text, 100);
+    cout << "Please Enter number to shift: ";
+    cin >> shift;
+}
+void encryption(char text[], int shift)
+{
+    for (int a = 0; text[a] != '\0'; a++)
+    {
+        if (text[a] > 65 && text[a] < 90)
+        {
+            text[a] = (text[a] - 'A' + shift) % 26 + 'A';
+        }
+        else
+            text[a] = (text[a] - 'a' + shift) % 26 + 'a';
+    }
+        for (int a = 0; text[a] != '\0'; a++)
+        {
+            cout << text[a];
+        }
 }
